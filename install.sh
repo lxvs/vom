f3a2e92695d31f6e261f4dd3e78b6ad0f687e90f
lxvs <jn.apsd@gmail.com>
2022-07-10 20:54:22 +0800

    Register vimom (Vim on Mintty) for some file types

---
+++ install.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+set -o nounset
+set -o noglob
+
+die () {
+    while test $# -ge 1
+    do
+        printf >&2 "%s\n" "$1"
+        shift
+    done
+    exit 1
+}
+
+RegFileTypes () {
+    local ft
+    local icon='%SystemRoot%\system32\imageres.dll,-102'
+    for ft in "${!ftt[@]}"
+    do
+        printf "Register file type \`%s'\n" "$ft"
+        reg add "$classes\\vimom.$ft" //ve //t "$t_sz" //d "${ftt[$ft]}" //f 1>/dev/null || return
+        reg add "$classes\\vimom.$ft\\DefaultIcon" //ve //t "$t_ex" //d "$icon" //f 1>/dev/null || return
+        reg add "$classes\\vimom.$ft\\shell\\open" //v "Icon" //t "$t_ex" //d "$mintty" //f 1>/dev/null || return
+        reg add "$classes\\vimom.$ft\\shell\\open\\command" //ve //t "$t_ex" //d "$command" //f 1>/dev/null || return
+        reg add "$classes\\.$ft" //ve //t "$t_sz" //d "vimom.$ft" //f 1>/dev/null || return
+        reg add "$classes\\.$ft\\OpenWithProgids" //v "vimom.$ft" //t "$t_none" //f 1>/dev/null || return
+    done
+}
+
+RegProgram () {
+    local text="Edit &with Vim on Mintty"
+    printf "Register program\n"
+    reg add "$classes\\*\\shell\\vimom" //ve //t "$t_sz" //d "$text" //f 1>/dev/null || return
+    reg add "$classes\\*\\shell\\vimom" //v "Icon" //t "$t_ex" //d "$mintty" //f  1>/dev/null || return
+    reg add "$classes\\*\\shell\\vimom\\command" //ve //t "$t_ex" //d "$command" //f 1>/dev/null || return
+}
+
+InitFileTypeTable () {
+    ftt=( \
+        ['asl']='ACPI Source Language File' \
+        ['ass']='ASS Subtitle File' \
+        ['bash']='Bash Script' \
+        ['bashrc']='Bash Run Command Script' \
+        ['bash_login']='Bash Login Script' \
+        ['bash_logout']='Bash Logout Script' \
+        ['bash_profile']='Bash Profile Script' \
+        ['c']='C Source File' \
+        ['c++']='C++ Source File' \
+        ['cfg']='Configuration File' \
+        ['conf']='Configuration File' \
+        ['config']='Configuration File' \
+        ['cpp']='C++ Source File' \
+        ['cs']='C# Source File' \
+        ['css']='Cascading Style Sheet File' \
+        ['csv']='Comma Separated Values File' \
+        ['diff']='Diff File' \
+        ['dockerfile']='Dockerfile Source File' \
+        ['editorconfig']='Editor Configuration File' \
+        ['git']='git Source File' \
+        ['gitattributes']='Git Attributes File' \
+        ['gitconfig']='Git Configuration File' \
+        ['gitignore']='Git Ignore File' \
+        ['go']='Go Source File' \
+        ['groovy']='Groovy Source File' \
+        ['h']='C Header Source File' \
+        ['h++']='C++ Header Source File' \
+        ['hpp']='C++ Header Source File' \
+        ['htm']='HTML Source File' \
+        ['html']='HTML Source File' \
+        ['ini']='INI Configuration File' \
+        ['java']='Java Source File' \
+        ['js']='JavaScript Source File' \
+        ['json']='JavaScript Object Notation File' \
+        ['log']='Log File' \
+        ['lua']='Lua Source File' \
+        ['makefile']='Makefile Source File' \
+        ['markdown']='Markdown File' \
+        ['md']='Markdown File' \
+        ['nsh']='EFI Shell Script' \
+        ['php']='PHP Source File' \
+        ['profile']='Profile Script' \
+        ['ps1']='PowerShell Source File' \
+        ['py']='Python Script' \
+        ['pyi']='Python Stub File' \
+        ['sh']='Shell Script' \
+        ['shtml']='SHTML Source File' \
+        ['srt']='SubRip Subtitle File' \
+        ['ssa']='Sub Station Alpha Subtitle File' \
+        ['txt']='Text Document' \
+        ['vb']='vb Source File' \
+        ['vim']='Vimscript Source File' \
+        ['vimrc']='Vim Run Command File' \
+        ['xml']='Extensible Markup Language File' \
+        ['yaml']='YAML File' \
+        ['yml']='YAML File' \
+        ['zsh']='Zsh Script' \
+    )
+}
+
+Register () {
+    local classes='HKCU\SOFTWARE\Classes'
+    local mintty='%ProgramFiles%\Git\usr\bin\mintty.exe'
+    local minttycmdflag='--title "%1" /bin/bash --login -c "/usr/bin/vim -- \"%1\""'
+    local command="$mintty $minttycmdflag"
+    local t_sz="REG_SZ" t_ex="REG_EXPAND_SZ" t_none="REG_NONE"
+    local -A ftt
+    InitFileTypeTable || return
+    RegProgram || return
+    RegFileTypes || return
+}
+
+IsOnWindows () {
+    grep -Gqi '^win' <<<"${OS-}"
+}
+
+main () {
+    IsOnWindows && Register
+}
+
+main "$@"
